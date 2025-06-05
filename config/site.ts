f8c510dc52416be5ff93e52f184bc3c3e9b67f34
birminghambusinesscentre <birminghambusinesscentrephotos@gmail.com>
2025-06-05 18:45:22 -0400

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Poem Generator",
+  description: "Generate poems with AI",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+};
